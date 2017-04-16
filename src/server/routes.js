9b899a6256d625d0648465805693118a9aee03fe
Pavel Smirnov <mrpsmirnov@yandex.ru>
2017-04-16 10:30:54 +0300

    Add update handling

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 import express from 'express';
 import mongoose from 'mongoose';
 
@@ -29,13 +31,63 @@ apiRouter.route('/questions')
     });
   });
 
-apiRouter.route('/questions/:questionId').get((req, res) => {
+apiRouter.use('/questions/:questionId', (req, res, next) => {
   Question.findById(req.params.questionId, (err, question) => { //eslint-disable-line
     if (err) {
       res.status(500).send(err);
+      return;
     }
-    res.json(question);
+    if (question) {
+      req.question = question;
+      next();
+      return;
+    }
+    res.status(404).send('no question found');
   });
 });
+
+apiRouter.route('/questions/:questionId')
+  .get((req, res) => {
+    res.json(req.question);
+  })
+  .put((req, res) => {
+    req.question.exam = req.body.exam;
+    req.question.type = req.body.type;
+    req.question.title = req.body.title;
+    req.question.options = req.body.options;
+
+    req.question.save((err) => { //eslint-disable-line
+      if (err) {
+        res.status(500).send(err);
+        return;
+      }
+      res.json(req.question);
+    });
+  })
+  .patch((req, res) => {
+    Object.keys(req.body).map((key) => {
+      if (key !== '_id') {
+        req.question[key] = req.body[key];
+      }
+      return key;
+    });
+
+    req.question.save((err) => { //eslint-disable-line
+      if (err) {
+        res.status(500).send(err);
+        return;
+      }
+      res.json(req.question);
+    });
+  })
+  .delete((req, res) => {
+    req.question.remove((err) => {
+      if (err) {
+        res.status(500).send(err);
+        return;
+      }
+      res.status(204).send('Removed');
+    });
+  });
 
 export default apiRouter;
